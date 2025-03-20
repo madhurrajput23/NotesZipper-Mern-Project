94979f2c73f88fa382858037f58908fff6d761da
Madhur Kumar <madhurrajput23.com>
2025-03-20 23:25:31 +0530

    Initial commit - Added frontend and backend

---
+++ backend/data/notes.js
@@ -0,0 +1,30 @@
+const notes = [
+  {
+    _id: "1",
+    title: "Day 1 of the college",
+    content:
+      "I made a few new friends and introduced myself to a lot of new people",
+    category: "College",
+  },
+  {
+    _id: "2",
+    title: "Learned some Node JS",
+    content: "Learned how to create a server in node JS and my first API",
+    category: "Learning",
+  },
+  {
+    _id: "3",
+    title: "Learned some MongoDB",
+    content: "Learning MongoDB",
+    category: "Learning",
+  },
+  {
+    _id: "4",
+    title: "Started React JS",
+    content:
+      "Made my first App in React JS, feels awesome to learn something new.",
+    category: "Learning",
+  },
+];
+
+module.exports = notes;
